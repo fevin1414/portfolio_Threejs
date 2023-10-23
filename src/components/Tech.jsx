5ba21e4b7f82c0cd64f70ebb5d07302e90624122
fevin1414 <fevinbiju1414@gmail.com>
2023-10-23 09:18:16 -0500

    mobile fix test 4

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -6,22 +6,21 @@ import { technologies } from "../constants";
 
 const Tech = () => {
   return (
-    <div className='flex flex-col items-center gap-5'>
+    <div className="flex flex-col items-center gap-5">
       <div>
-      <p className={styles.sectionSubText}>Tools/frameworks</p>
+        <p className={styles.sectionSubText}>Tools/frameworks</p>
         <h2 className={styles.sectionHeadText}>Skills</h2>
-
       </div>
-      <div className='flex flex-row flex-wrap justify-center gap-10'>
-        {technologies.map((technology) => (
+      <div className="flex flex-row flex-wrap justify-center gap-10">
+        {/* {technologies.map((technology) => (
           <div className='w-28 h-28' key={technology.name}>
             <BallCanvas icon={technology.icon} />
           </div>
-        ))}
+        ))} */}
+        <p>All my techs</p>
       </div>
     </div>
   );
 };
-
 
 export default SectionWrapper(Tech, "");
