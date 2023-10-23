c66a5bce5d0f8a2c9d92183767c4ddace93ba35c
fevin1414 <fevinbiju1414@gmail.com>
2023-10-23 09:05:05 -0500

    mobile fix 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,41 @@
 import { BrowserRouter } from "react-router-dom";
-import {About,Contact,Experience,Feedbacks,Hero, Navbar,Tech,Works, StarsCanvas} from './components'
+import {
+  About,
+  Contact,
+  Experience,
+  Feedbacks,
+  Hero,
+  Navbar,
+  Tech,
+  Works,
+  StarsCanvas,
+} from "./components";
 
-
-  const App = () => {
-
+const App = () => {
   return (
-   <BrowserRouter>
-   <div className="relative z-0 bg-primary">
-    <div className="bg-hero-pattern bg-cover
-    bg-no-repeat bg-center">
-    <Navbar />
-    <Hero />
-
-    </div>
-    <About />
-    <Experience />
-    <Tech />
-    <Works />
-    <Feedbacks />
-    <div>
-      <div className="relative z-0">
-        <Contact />
-        <StarsCanvas />
+    <BrowserRouter>
+      <div className="relative z-0 bg-primary">
+        <div
+          className="bg-hero-pattern bg-cover
+    bg-no-repeat bg-center"
+        >
+          <Navbar />
+          <Hero />
+        </div>
+        <About />
+        <Experience />
+        <Tech />
+        <Works />
+        <Feedbacks />
+        <div>
+          <div className="relative z-0">
+            <Contact />
+            {/* <StarsCanvas /> */}
+          </div>
+        </div>
       </div>
-    </div>
-    </div>
     </BrowserRouter>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,7 +8,7 @@ import StarsCanvas from "./canvas/Stars";
 const Hero = () => {
   return (
     <section className="relative w-full h-screen mx-auto ">
-      <StarsCanvas />
+      {/* <StarsCanvas /> */}
 
       <div
         className={`absolute inset-0 top-[120px]  max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}
