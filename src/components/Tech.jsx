87fc9da18e46785af37f5f68ab8b31401aa2a373
fevin1414 <fevinbiju1414@gmail.com>
2023-10-23 10:00:04 -0500

    fixed

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -3,19 +3,33 @@ import { styles } from "../styles";
 import { BallCanvas } from "./canvas";
 import { SectionWrapper } from "../hoc";
 import { technologies } from "../constants";
+import { useMediaQuery } from "react-responsive";
+
+// Create a custom Card component for mobile view
+const MobileTechCard = ({ technology }) => (
+  <div className="w-28 h-28 p-4 bg-white rounded-lg shadow">
+    <BallCanvas icon={technology.icon} />
+  </div>
+);
 
 const Tech = () => {
+  const isMobile = useMediaQuery({ maxWidth: 767 });
+
   return (
     <div className="flex flex-col items-center gap-5">
       <div>
         <h2 className={styles.sectionHeadText}>Skills</h2>
       </div>
       <div className="flex flex-row flex-wrap justify-center gap-10">
-        {technologies.map((technology) => (
-          <div className="w-28 h-28" key={technology.name}>
-            <BallCanvas icon={technology.icon} />
-          </div>
-        ))}
+        {isMobile
+          ? technologies.map((technology) => (
+              <MobileTechCard key={technology.name} technology={technology} />
+            ))
+          : technologies.map((technology) => (
+              <div className="w-28 h-28" key={technology.name}>
+                <BallCanvas icon={technology.icon} />
+              </div>
+            ))}
       </div>
     </div>
   );
--- src/components/canvas/Ball.jsx
+++ src/components/canvas/Ball.jsx
@@ -1,39 +1,38 @@
 import React, { Suspense } from "react";
 import { Canvas } from "@react-three/fiber";
-import {
-  Decal,
-  Float,
-  OrbitControls,
-  Preload,
-  useTexture,
-} from "@react-three/drei";
-
 import CanvasLoader from "../Loader";
+import { Bloom, EffectComposer } from "@react-three/postprocessing";
+import { Float, OrbitControls, Preload, useTexture } from "@react-three/drei";
 
-const Ball = (props) => {
-  const [decal] = useTexture([props.imgUrl]);
+const Square = (props) => {
+  const texture = useTexture(props.imgUrl);
 
   return (
-    <Float speed={1.75} rotationIntensity={1} floatIntensity={2}>
+    <group>
       <ambientLight intensity={0.25} />
-      <directionalLight position={[0, 0, 0.05]} />
+      <directionalLight position={[0, 5, 0]} intensity={0.5} />
+      <pointLight
+        position={[0, -2.5, 0]}
+        intensity={1.5}
+        distance={3}
+        decay={2}
+        color="#fff8eb"
+      />
+
       <mesh castShadow receiveShadow scale={2.75}>
-        <icosahedronGeometry args={[1, 1]} />
-        <meshStandardMaterial
+        <boxGeometry args={[1, 1, 1]} />
+        <meshPhysicalMaterial
           color="#fff8eb"
           polygonOffset
           polygonOffsetFactor={-5}
           flatShading
-        />
-        <Decal
-          position={[0, 0, 1]}
-          rotation={[2 * Math.PI, 0, 6.25]}
-          scale={1}
-          map={decal}
-          flatShading
+          map={texture}
+          emissive="#fff8eb" // Glow color
+          emissiveIntensity={0.5} // Glow intensity
+          emissiveMap={texture} // Use the texture as an emissive map. It will glow in transparent parts.
         />
       </mesh>
-    </Float>
+    </group>
   );
 };
 
@@ -46,7 +45,15 @@ const BallCanvas = ({ icon }) => {
     >
       <Suspense fallback={<CanvasLoader />}>
         <OrbitControls enableZoom={false} />
-        <Ball imgUrl={icon} />
+        <Square imgUrl={icon} />
+
+        <EffectComposer>
+          <Bloom
+            luminanceThreshold={0.3}
+            luminanceSmoothing={0.8}
+            intensity={1.2}
+          />
+        </EffectComposer>
       </Suspense>
 
       <Preload all />
